96dfb3bd1c27332514a5029ec35e5053892a9791
MilagrosCabrera23 <milagrosmagalycabrera@gmail.com>
2026-04-08 09:59:19 -0300

    feat-finalizamos service de auth y corregimos url

--- login-frontend/src/app/core/services/auth.ts
+++ login-frontend/src/app/core/services/auth.ts
@@ -1,11 +1,25 @@
 import { Injectable,inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable,BehaviorSubject } from 'rxjs';
+import { Observable,BehaviorSubject,tap } from 'rxjs';
 import { environment } from '../../../environments/environment';
 
 export interface User {
-  id?: number;
+  id: number;
   email: string;
+  username: string;
+}
+export interface AuthResponse {
+access: string;
+refresh: string;
+user: User;
+}
+
+export interface OtpResponse {
+message: string;
+email: string;
+}
+export interface MessageResponse {
+  message: string;
 }
 
 @Injectable({
@@ -21,6 +35,41 @@ export class AuthService {
   
   public get currentUserValue(): User | null {
     return this.currentUserSubject.value;
+  }
+
+  login(username: string, password: string): Observable<AuthResponse> {
+    return this.http.post<AuthResponse>(`${this.apiUrl}/login/`, { username, password }).pipe(
+      tap(response => {
+        if (response && response.access) {
+      
+          localStorage.setItem('accessToken', response.access);
+          localStorage.setItem('refreshToken', response.refresh);
+          localStorage.setItem('currentUser', JSON.stringify(response.user));
+     
+          this.currentUserSubject.next(response.user);
+        }
+      })
+    );
+  }
+
+  logout(): void {
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('refreshToken');
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
+  }
+
+  getProfile(): Observable<User> {
+    return this.http.get<User>(`${this.apiUrl}/profile/`);
+  }
+
+  requestOtp(email: string): Observable<OtpResponse> {
+    return this.http.post<OtpResponse>(`${this.apiUrl}/password-reset/`, { email });
+  }
+
+  resetPassword(email: string, otp_code: string, new_password: string): Observable<MessageResponse> {
+    const body = { email, otp_code, new_password };
+    return this.http.post<MessageResponse>(`${this.apiUrl}/password-reset/confirm/`, body);
   }
 }
 
