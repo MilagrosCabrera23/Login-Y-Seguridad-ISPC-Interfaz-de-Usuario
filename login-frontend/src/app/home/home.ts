285bdffc63fc1d793210e8175c304a3ccf9f217f
MilagrosCabrera23 <milagrosmagalycabrera@gmail.com>
2026-04-10 10:13:01 -0300

    agregamos lo faltante de home

--- login-frontend/src/app/home/home.ts
+++ login-frontend/src/app/home/home.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component,inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+import { AuthService, User } from '../core/services/auth';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+
+export class Home {
+  private authService =  inject(AuthService);
+  private router =  inject(Router);
+
+  user$: Observable<User | null> = this.authService.currentUser$;
+  
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
